7c76178f3f5ff627771b534ca933ab089b97e0d9
nkmathew <kipkoechmathew+home@gmail.com>
2025-10-09 17:44:34 +0300

    [*] Migrate Android build to standard React Native setup and upgrade Kotlin to 2.1.0

--- kikuyu-flashcards-mobile/android/app/src/main/java/com/kikuyulanguagehub/flashcards/MainApplication.kt
+++ kikuyu-flashcards-mobile/android/app/src/main/java/com/kikuyulanguagehub/flashcards/MainApplication.kt
@@ -3,28 +3,32 @@ package com.kikuyulanguagehub.flashcards
 import android.app.Application
 import android.content.res.Configuration
 
-import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.common.ReleaseLevel
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.ReactHost
+import com.facebook.soloader.SoLoader
+import java.util.ArrayList
 
 // Removed Expo wrappers: using standard React Native host
 
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
-    override fun getPackages(): List<ReactPackage> =
-      PackageList(this).packages
+    override fun getPackages(): List<ReactPackage> {
+      val packages = ArrayList<ReactPackage>()
+      // Add all React Native packages here
+      return packages
+    }
 
     override fun getJSMainModuleName(): String = "index"
 
     override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-    override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+    override val isNewArchEnabled: Boolean = false // No new architecture support for now
   }
 
   // Bridgeless ReactHost override removed for standard ReactApplication setup
@@ -36,7 +40,7 @@ class MainApplication : Application(), ReactApplication {
     } catch (e: IllegalArgumentException) {
       ReleaseLevel.STABLE
     }
-    loadReactNative(this)
+    SoLoader.init(this, false)
   }
 
   override fun onConfigurationChanged(newConfig: Configuration) {
